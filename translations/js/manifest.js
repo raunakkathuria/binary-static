c100b1494d322ab2975e8b982c3fc24b59ab2ce3
ashkan <ashkan.nemati@gmail.com>
2018-04-06 10:39:36 +0200

    Release to translations

--- translations/js/manifest.js
+++ translations/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"aa808bbdef70799df454","2":"da59ab9411c9a17d8924","3":"67923bbb86d110fc7543"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"137400781a2f525c5063","2":"05d725a52b0b3465be05","3":"14af0cd43b3323575289"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
